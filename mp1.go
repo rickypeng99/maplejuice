856bd1e23dea1457ccfa9921301b827bbaf83310
Thomas Huang <qingyih2@illinois.edu>
2020-09-27 02:13:55 -0500

    make gossip heartbeat call sendrunning function

--- mp1.go
+++ mp1.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net"
 	"os"
@@ -12,7 +13,6 @@ import (
 	"strings"
 	"sync"
 	"time"
-	"log"
 )
 
 //lock
@@ -33,6 +33,7 @@ const (
 
 var NODES [NODE_CNT]string = makeNodes()
 var PORT string = "8000"
+
 // CONSTANT VARIABLES
 const (
 	// heartbeating style (mode)
@@ -48,12 +49,12 @@ const (
 	FAILED_REMOVAL string = "FAILURE_REMOVAL"
 
 	// messageType
-	INITIALIZED string = "INITIALIZED"
-	HEARTBEAT   string = "HEARTBEAT"
-	JOIN        string = "JOIN"
-	LEAVE       string = "LEAVE"
-	CHANGE      string = "CHANGE"
-	FAILURE     string = "FAILURE"
+	INITIALIZED     string = "INITIALIZED"
+	HEARTBEAT       string = "HEARTBEAT"
+	JOIN            string = "JOIN"
+	LEAVE           string = "LEAVE"
+	CHANGE          string = "CHANGE"
+	FAILURE         string = "FAILURE"
 	FAILURE_REMOVED string = "FAILURE_REMOVED"
 	// SUSPECTION  string = "SUSPECTION"
 )
@@ -101,7 +102,7 @@ func main() {
 	// if err != nil {
 	// 	log.Printf("os.HostName() err")
 	// }
-	hostname := "127.0.0.1:" + PORT 
+	hostname := "127.0.0.1:" + PORT
 
 	// setting up current server struct
 	var server_temp Server
@@ -168,6 +169,8 @@ func commandReader(server *Server) {
 		case "id":
 			// show current id
 			fmt.Printf("Id for the current node is: %d\n", server.Id)
+		case "mode":
+			fmt.Printf("Current Heartbeat Mode is: %v\n", server.Mode)
 		}
 	}
 }
@@ -183,7 +186,7 @@ func messageListener(server *Server) {
 	addrinfo := net.UDPAddr{
 		// IP:   net.ParseIP(server.Hostname),
 		// Port: port_string,
-		IP: net.ParseIP("localhost"),
+		IP:   net.ParseIP("localhost"),
 		Port: port_string,
 	}
 
@@ -206,14 +209,13 @@ func messageListener(server *Server) {
 
 func messageHandler(server *Server, resp []byte, bytes_read int) {
 	message := unmarshalMsg([]byte(string(resp[:bytes_read])))
-	// fmt.Println(message)
-	if message.Mode != server.Mode {
+	// log.Printf("Received message type :%v from host:%v", message.MessageType, message.Hostname)
+	if message.Mode != server.Mode && message.MessageType != CHANGE {
 		// ignore packets from different mode
 		return
 	}
 
 	if message.MessageType == HEARTBEAT {
-		log.Printf("Received HEARTBEAT from host:%s", message.Hostname)
 		heartBeatHandler(server, message)
 	} else if message.MessageType == INITIALIZED {
 		// Sent from the introducer to the new joined node
@@ -243,7 +245,7 @@ func messageHandler(server *Server, resp []byte, bytes_read int) {
 				// socket, err := net.Dial("udp", hostname+":"+PORT)
 				socket, err := net.Dial("udp", hostname)
 				if err != nil {
-					log.Printf("Error: dialing UDP from introducer to normal nodes")
+					log.Printf("Error: dialing UDP from node: %s to new joined node", server.Hostname)
 				}
 				var message Message = Message{
 					MessageType:   JOIN,
@@ -268,7 +270,7 @@ func messageHandler(server *Server, resp []byte, bytes_read int) {
 			log.Printf("%s has successfully joined the group", message.Hostname)
 
 			if err != nil {
-				log.Printf("Error: dialing UDP from introducer to new joined node")
+				log.Printf("Error: dialing UDP from node: %s to : %s when sending initialized data", server.Hostname, message.Hostname)
 			}
 			var message Message = Message{
 				MessageType:   INITIALIZED,
@@ -286,7 +288,6 @@ func messageHandler(server *Server, resp []byte, bytes_read int) {
 			}
 		}
 
-
 	} else if message.MessageType == CHANGE {
 		prev_mode := server.Mode
 		if server.Mode == GOSSIP {
@@ -303,7 +304,7 @@ func messageHandler(server *Server, resp []byte, bytes_read int) {
 			return
 		} else if server.MembershipMap[message.Hostname].Status == RUNNING {
 			server.MembershipMap[message.Hostname].Status = FAILED
-		} 
+		}
 
 		// sending messages of failure or suspected
 		temp := NODES
@@ -331,39 +332,41 @@ func sendHeartbeat(server *Server) {
 	if server.Mode == GOSSIP {
 		/*GOSSIP HEARTBEAT*/
 		//select a random receiver
+		// TODO : make sure only send to living nodes
 		var temp []string = getAllNodesButSelf(NODES[:], server)
 		rand.Shuffle(len(temp), func(i, j int) {
 			temp[i], temp[j] = temp[j], temp[i]
 		})
 		rand_Nodes := temp[0:GOSSIP_PARA]
+		sendRunning(server, HEARTBEAT, server.Hostname, rand_Nodes)
 		// fmt.Println(rand_Nodes)
 
 		mutex.Lock()
 		server.MembershipMap[server.Hostname].Heartbeat += 1
 		server.MembershipMap[server.Hostname].Timestamp = time.Now()
-		for _, node := range rand_Nodes {
-			// socket, err := net.Dial("udp", node+":"+PORT)
-			socket, err := net.Dial("udp", node)
+		// for _, node := range rand_Nodes {
+		// 	// socket, err := net.Dial("udp", node+":"+PORT)
+		// 	socket, err := net.Dial("udp", node)
 
-			if err != nil {
-				log.Printf("Error: dialing UDP from introducer to normal nodes")
-			}
-			var message Message = Message{
-				MessageType:   HEARTBEAT,
-				Mode:          server.Mode,
-				Hostname:      server.Hostname,
-				MembershipMap: dereferencedMemebershipMap(server.MembershipMap),
-			}
+		// 	if err != nil {
+		// 		log.Printf("Error: dialing UDP from node: %s to : %s", server.Hostname, node)
+		// 	}
+		// 	var message Message = Message{
+		// 		MessageType:   HEARTBEAT,
+		// 		Mode:          server.Mode,
+		// 		Hostname:      server.Hostname,
+		// 		MembershipMap: dereferencedMemebershipMap(server.MembershipMap),
+		// 	}
 
-			//marshal the message to json
-			var marshaledMsg []byte = marshalMsg(message)
-			// fmt.Println(string(marshaledMsg))
-			// write to the socket
-			_, err = socket.Write(marshaledMsg)
-			if err != nil {
-				log.Printf("Error: Writing JOIN message to the socket: %s", err)
-			}
-		}
+		// 	//marshal the message to json
+		// 	var marshaledMsg []byte = marshalMsg(message)
+		// 	// fmt.Println(string(marshaledMsg))
+		// 	// write to the socket
+		// 	_, err = socket.Write(marshaledMsg)
+		// 	if err != nil {
+		// 		log.Printf("Error: Writing JOIN message to the socket: %s", err)
+		// 	}
+		// }
 		mutex.Unlock()
 	} else {
 		/*ALL_TO_ALL_HEARTBEAT*/
@@ -377,11 +380,11 @@ func sendHeartbeat(server *Server) {
  * monitor function invocked immediatly after server send a heartbeat
  * manage membership list status according to heartbeat mode
  */
-func monitor(server *Server) {	
+func monitor(server *Server) {
 	var failed []string
 
 	interval := time.Tick(BEAT_PERIOD)
-	for range interval{
+	for range interval {
 		if server.Mode == GOSSIP {
 			for _, node := range server.MembershipMap {
 				// if running and time out
@@ -536,7 +539,7 @@ func merge(self map[string]*Member, other map[string]Member) {
 			// fmt.Printf("self: %d\n", self[key].Heartbeat)
 
 			if member.Heartbeat > self[key].Heartbeat {
-				fmt.Println("yes")
+				// fmt.Println("yes")
 				self[key].Heartbeat = member.Heartbeat
 				self[key].Timestamp = time.Now()
 				self[key].Status = RUNNING
@@ -575,7 +578,7 @@ func makeNodes() [10]string {
 	// 	}
 	// }
 	for idx, _ := range result {
-		result[idx] = "127.0.0.1:" + strconv.Itoa(8000 + idx)
+		result[idx] = "127.0.0.1:" + strconv.Itoa(8000+idx)
 	}
 	return result
 }
@@ -585,13 +588,14 @@ func makeNodes() [10]string {
  */
 func sendRunning(server *Server, msgType string, msgHostName string, msgDst []string) {
 	for hostname, _ := range msgDst {
-		//fmt.Println(*(server.MembershipMap[msgDst[hostname]]))
+		fmt.Println(*(server.MembershipMap[msgDst[hostname]]))
+		fmt.PRintf("sending a msg type: %v in sendrunning" msgType)
 		if server.Hostname != msgDst[hostname] && server.MembershipMap[msgDst[hostname]].Status == RUNNING {
 			// socket, err := net.Dial("udp", msgDst[hostname]+":"+PORT)
-			socket, err := net.Dial("udp",  msgDst[hostname])
+			socket, err := net.Dial("udp", msgDst[hostname])
 
 			if err != nil {
-				log.Printf("Error: dialing UDP from introducer to normal nodes")
+				log.Printf("Error: dialing UDP from to : %s in sendRunning", server.Hostname)
 			}
 			var message Message = Message{
 				MessageType:   msgType,
@@ -644,11 +648,11 @@ func timer(server *Server) {
 }
 
 func getAllNodesButSelf(vs []string, server *Server) []string {
-    vsf := make([]string, 0)
-    for _, v := range vs {
-        if v != server.Hostname {
-            vsf = append(vsf, v)
-        }
-    }
-    return vsf
+	vsf := make([]string, 0)
+	for _, v := range vs {
+		if v != server.Hostname {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
 }
