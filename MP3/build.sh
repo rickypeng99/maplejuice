67c79ea2929163ca601e056563209a684b989c51
rickyrpeng <rickyrpeng@tencent.com>
2020-11-29 20:23:25 +0800

    update build

--- MP3/build.sh
+++ MP3/build.sh
@@ -4,3 +4,8 @@ go build ./applications/wordFreq_maple.go
 go build ./applications/wordFreq_juice.go
 mv wordFreq_maple ./applications
 mv wordFreq_juice ./applications
+if [$HOSTNAME = 'fa20-cs425-g35-01.cs.illinois.edu']
+then
+    go build generateInput.go
+    ./generateInput
+fi
--- MP3/master_build.sh
+++
@@ -1,2 +0,0 @@
-go build generateInput.go
-./generateInput
